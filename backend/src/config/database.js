a397b4ed87558d29e427ce68b0d80522055a1497
Saul Garcia <saulgf12@gmail.com>
2025-08-23 23:09:31 -0600

    chore: eliminar carga de dotenv

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -24,6 +24,13 @@ const testConnection = async () => {
     return true
   } catch (error) {
     console.error('❌ Error conectando a MySQL:', error.message)
+    console.error('❌ variables de entorno:', {
+      DB_HOST: process.env.DB_HOST,
+      DB_PORT: process.env.DB_PORT,
+      DB_USER: process.env.DB_USER,
+      DB_PASSWORD: process.env.DB_PASSWORD,
+      DB_NAME: process.env.DB_NAME
+    })
     return false
   }
 }
